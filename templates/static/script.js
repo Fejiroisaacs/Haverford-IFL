1886d5be42e95a27247269baf3393439b69b1e1c
Oghenefejiro Anigboro <Fejiroisaac@gmail.com>
2024-12-03 13:47:11 -0500

    updated scrolling

--- templates/static/script.js
+++ templates/static/script.js
@@ -2,36 +2,23 @@ window.addEventListener('scroll', function() {
     let header = document.querySelector('header');
     let windowPosition = window.scrollY > 0;
     header.classList.toggle('scrolling-active', windowPosition);
-})
+});
 
-//w3schools code here
-/* Open when someone clicks on the span element */
+// Navigation functions
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
 }
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
 
 function openMobileNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("navIcon").style.display = "None";
 }
 
 function closeMobileNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("navIcon").style.display = "Block";
 }
-//end of w3schools code
-
-// '.tbl-content' consumed little space for vertical scrollbar, scrollbar width depend on browser/os/platfrom. Here calculate the scollbar width .
-$(window).on("load resize ", function() {
-    var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
-    $('.tbl-header').css({'padding-right':scrollWidth});
-  }).resize();
-
-  window.addEventListener('scroll', function() {
-    let header = document.querySelector('header');
-    let windowPosition = window.scrollY > 0;
-    header.classList.toggle('scrolling-active', windowPosition);
-})
