9a194b79b1592c4b5b8653b08e9b9f2b4ef3f33c
Oghenefejiro Anigboro <Fejiroisaac@gmail.com>
2024-12-31 20:17:36 -0500

    updated teams ui and backend added ind team page

--- routers/teams.py
+++ routers/teams.py
@@ -1,30 +1,30 @@
-from fastapi import FastAPI, Request, Depends, HTTPException, Form
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import APIRouter, Request, Cookie, Depends
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel
-import firebase_admin
-from fastapi.responses import RedirectResponse
-from starlette.responses import HTMLResponse, RedirectResponse
-import pyrebase, json
-from fastapi import APIRouter
+from firebase_admin import db
+from starlette.responses import HTMLResponse
 
 router = APIRouter()
-
 templates = Jinja2Templates(directory="templates")
 user = None
-firebase_config = json.load(open("cred.json"))
-
-firebase = pyrebase.initialize_app(firebase_config)
-db = firebase.database() 
-teams = db.child("Teams").get()
-teams = {team.key(): team.val() for team in teams}
 
 @router.get("/teams", response_class=HTMLResponse)
-async def read_players(request: Request):
+async def teams_home(request: Request, session_token: str = Cookie(None), db: db.Reference = Depends(lambda: db.reference('/'))):
     return templates.TemplateResponse("teams.html", {"request": request, "user": user, "Teams": [], "count": 0})
 
+@router.get("/teams/{team}", response_class=HTMLResponse)
+async def get_team(request: Request, team: str, db: db.Reference = Depends(lambda: db.reference('/'))):
+    teams_ref = db.child('Teams')
+    team_data = teams_ref.child(team).get()
+    if team_data:
+        return templates.TemplateResponse("team.html", {"request": request, "user": user, "team": team, "data": team_data})
+    else:
+        return templates.TemplateResponse("404error.html", {"request": request})
+
 @router.get("/team_search", response_class=HTMLResponse)
-async def search_players(request: Request, query: str):
-    filtered_players = [teams[team] for team in teams if query.lower() in team.lower()]
-    return templates.TemplateResponse("teams.html", {"request": request, "user": user, "Teams": filtered_players, "count": len(filtered_players)})
+async def search_teams(request: Request, query: str, db: db.Reference = Depends(lambda: db.reference('/'))):
+    teams_ref = db.child('Teams')
+    teams = teams_ref.get()
+    filtered_teams = {k: v for k, v in teams.items() if query.lower() in k.lower()}
+    filtered_teams = [teams[team] for team in filtered_teams]
+    
+    return templates.TemplateResponse("teams.html", {"request": request, "user": user, "Teams": filtered_teams, "count": len(filtered_teams)})
