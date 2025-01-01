9f02020b9429d84e5d0c6b11b7e8f92adb6a383d
Oghenefejiro Anigboro <Fejiroisaac@gmail.com>
2024-12-31 20:16:03 -0500

    updated main app logic

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 from fastapi import FastAPI, Request, Depends, HTTPException, Form, status
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel
 import firebase_admin
-from fastapi.responses import RedirectResponse
-from starlette.responses import HTMLResponse, RedirectResponse
-import pyrebase, json
+from firebase_admin import credentials, auth, storage, db
+from starlette.responses import HTMLResponse
 from routers import matches, signup, login, contact, fantasy, players, settings, teams
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
+import json, os
+from starlette.middleware.sessions import SessionMiddleware
+from dotenv import load_dotenv
+
+load_dotenv()
 
 firebase_config = json.load(open("cred.json"))
+cred = credentials.Certificate(firebase_config)
+firebase_admin.initialize_app(cred, {
+    'storageBucket': os.getenv("storageBucket"),
+    'databaseURL': os.getenv("databaseURL")
+})
 
-firebase = pyrebase.initialize_app(firebase_config)
-db = firebase.database()
-fb_storage = firebase.storage()
-auth = firebase.auth()
+# Initialize Realtime Database and Storage
+db = db.reference('/')
+bucket = storage.bucket()
+
+secret_key = os.getenv("SECRET_KEY")
 
 app = FastAPI()
 
@@ -25,17 +34,17 @@ app.mount("/static", StaticFiles(directory="templates/static"), name="static")
 user = None
 templates = Jinja2Templates(directory="templates")
 
-app.include_router(matches.router)
-app.include_router(signup.router)
-app.include_router(login.router)
-app.include_router(contact.router)
-app.include_router(fantasy.router)
-app.include_router(players.router)
-app.include_router(settings.router)
-app.include_router(teams.router)
+app.add_middleware(SessionMiddleware, secret_key=secret_key)
 
-class User(BaseModel):
-    email: str
+app.include_router(matches.router, dependencies=[Depends(lambda: db)])
+app.include_router(signup.router, dependencies=[Depends(lambda: db), Depends(lambda: auth)])
+app.include_router(login.router, dependencies=[Depends(lambda: db), Depends(lambda: auth)])
+app.include_router(contact.router)
+app.include_router(fantasy.router, dependencies=[Depends(lambda: db), Depends(lambda: auth)])
+app.include_router(players.router, dependencies=[Depends(lambda: db)])
+app.include_router(settings.router)
+app.include_router(teams.router, dependencies=[Depends(lambda: db)])
+
 
 @app.exception_handler(StarletteHTTPException)
 async def http_exception_handler(request, exc):
@@ -43,30 +52,17 @@ async def http_exception_handler(request, exc):
         return templates.TemplateResponse("404error.html", {"request": request, "error": f"{exc.status_code} {str(exc.detail)}"})
     return templates.TemplateResponse("error.html", {"request": request, "error": str(exc.detail)})
 
-
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request, exc):
     if exc.status_code == 404:
         return templates.TemplateResponse("404error.html", {"request": request, "error": f"{exc.status_code} {str(exc.detail)}"})
     return templates.TemplateResponse("error.html", {"request": request, "error": str(exc.detail)})
 
-
-@app.post("/token")
-async def token():
-    # Handle user authentication with Firebase here
-    pass
-
 @app.get("/", response_class=HTMLResponse)
 @app.get("/index", response_class=HTMLResponse)
 @app.get("/home", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "user": user})
-
-@app.get("/logout", response_class=HTMLResponse)
-async def get_logout(request: Request):
-    global user
-    user = None
-    return RedirectResponse("/", status_code=303)
 
 if __name__ == "__main__":
     import uvicorn
