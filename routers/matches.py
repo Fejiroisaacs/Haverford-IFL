8a276b443f342f2720a177703a2016703ec37504
Oghenefejiro Anigboro <Fejiroisaac@gmail.com>
2025-01-02 22:29:41 -0500

    updated data fetchching logic

--- routers/matches.py
+++ routers/matches.py
@@ -1,25 +1,43 @@
-from fastapi import FastAPI, Request, Depends, HTTPException, Form
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import Request, Depends, Form
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel
-import firebase_admin
-from fastapi.responses import RedirectResponse
-from starlette.responses import HTMLResponse, RedirectResponse
-import pyrebase, pandas as pd
+from starlette.responses import HTMLResponse
+import pandas as pd
 from fastapi import APIRouter
 
 router = APIRouter()
 
 templates = Jinja2Templates(directory="templates")
 user = None
+CURRENT_SEASON = max(pd.read_csv("data/season_standings.csv")['Season'].tolist())
 
 @router.get("/matches", response_class=HTMLResponse)
 async def read_matches(request: Request):
-    return templates.TemplateResponse("matches.html", {"request": request, "user": user, "groups": get_table()})
+    return templates.TemplateResponse("matches.html", {"request": request, 
+                                                       "user": user, 
+                                                       "groups": get_table(CURRENT_SEASON),
+                                                       "active_season": CURRENT_SEASON,
+                                                       "current_season": CURRENT_SEASON})
 
-def get_table():
-    data = pd.read_csv("data/table.csv").to_dict(orient='records')
+@router.get("/matches/{season}", response_class=HTMLResponse)
+async def read_matches(request: Request, season: int):
+    if season <= CURRENT_SEASON and season > 0:
+        return templates.TemplateResponse("matches.html", {"request": request, 
+                                                        "user": user, 
+                                                        "groups": get_table(season),
+                                                        "active_season": season,
+                                                        "current_season": CURRENT_SEASON})
+    else:
+        return templates.TemplateResponse("matches.html", {"request": request, 
+                                                        "user": user, 
+                                                        "groups": get_table(CURRENT_SEASON),
+                                                        "active_season": CURRENT_SEASON,
+                                                        "current_season": CURRENT_SEASON})
 
-    return [data, data]
+def get_table(season):
+    data = pd.read_csv("data/season_standings.csv")
+    data = data[data['Season'] == season]
+    groupA = data[data["Group"] == 'A'].to_dict(orient='records')
+    groupB = data[data["Group"] == 'B'].to_dict(orient='records')
+    
+    return [groupA, groupB]
     
--- routers/teams.py
+++ routers/teams.py
@@ -1,30 +1,97 @@
 from fastapi import APIRouter, Request, Cookie, Depends
 from fastapi.templating import Jinja2Templates
-from firebase_admin import db
+from firebase_admin import db as firebase_db
 from starlette.responses import HTMLResponse
+import pandas as pd
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 user = None
+CURRENT_SEASON = max(pd.read_csv("data/season_standings.csv")['Season'].tolist())
+seasons_played = None
 
 @router.get("/teams", response_class=HTMLResponse)
-async def teams_home(request: Request, session_token: str = Cookie(None), db: db.Reference = Depends(lambda: db.reference('/'))):
+async def teams_home(request: Request, session_token: str = Cookie(None), db: firebase_db.Reference = Depends(lambda: firebase_db.reference('/'))):
     return templates.TemplateResponse("teams.html", {"request": request, "user": user, "Teams": [], "count": 0})
 
 @router.get("/teams/{team}", response_class=HTMLResponse)
-async def get_team(request: Request, team: str, db: db.Reference = Depends(lambda: db.reference('/'))):
+async def team_page(request: Request, team: str, db: firebase_db.Reference = Depends(lambda: firebase_db.reference('/'))):
     teams_ref = db.child('Teams')
     team_data = teams_ref.child(team).get()
     if team_data:
-        return templates.TemplateResponse("team.html", {"request": request, "user": user, "team": team, "data": team_data})
+        standings_data = get_standings(team)
+        matches_data = get_matches(team)
+        players_data = get_players(team)
+        seasons_played = list(standings_data.keys())
+
+        return templates.TemplateResponse("team.html", {
+            "request": request, 
+            "team": team, 
+            "data": team_data,
+            "standings_data": standings_data,
+            "matches_data": matches_data,
+            "players_data": players_data,
+            "seasons_played": seasons_played
+        })
     else:
         return templates.TemplateResponse("404error.html", {"request": request})
 
 @router.get("/team_search", response_class=HTMLResponse)
-async def search_teams(request: Request, query: str, db: db.Reference = Depends(lambda: db.reference('/'))):
+async def search_teams(request: Request, query: str, db: firebase_db.Reference = Depends(lambda: firebase_db.reference('/'))):
     teams_ref = db.child('Teams')
     teams = teams_ref.get()
     filtered_teams = {k: v for k, v in teams.items() if query.lower() in k.lower()}
     filtered_teams = [teams[team] for team in filtered_teams]
     
-    return templates.TemplateResponse("teams.html", {"request": request, "user": user, "Teams": filtered_teams, "count": len(filtered_teams)})
+    return templates.TemplateResponse("teams.html", {"request": request, 
+                                                     "user": user, "Teams": filtered_teams, 
+                                                     "count": len(filtered_teams)})
+
+def get_standings(team):
+    global seasons_played
+    data = pd.read_csv("data/season_standings.csv")
+    seasons_played = data[data['Team'] == team]['Season'].to_list()
+    team_data = {}
+    
+    for season in seasons_played:
+        sub_data = data[data['Season'] == season]
+        table = None
+        
+        groupA = sub_data[sub_data["Group"] == 'A']
+        groupB = sub_data[sub_data["Group"] == 'B']
+        
+        if groupA['Team'].isin([team]).any():
+            table = groupA.to_dict(orient='records')
+        elif groupB['Team'].isin([team]).any():
+            table = groupB.to_dict(orient='records')
+            
+        if table: team_data[season] = table
+        
+    return team_data
+
+def get_matches(team):
+    global seasons_played
+    data = pd.read_csv("data/Match_Results.csv")
+    match_data = {}
+    
+    for season in seasons_played:
+        sub_data = data[data['Season'] == season]
+        sub_data = sub_data[(sub_data['Team 1'] == team) | (sub_data['Team 2'] == team)].to_dict(orient='records')
+        
+        if sub_data: match_data[season] = sub_data
+    
+    return match_data
+
+def get_players(team):
+    global seasons_played
+    data = pd.read_csv("data/season_player_stats.csv")
+    players_data = {}
+    
+    for season in seasons_played:
+        sub_data:pd.DataFrame = data[data['Season'] == season]
+        sub_data = sub_data[sub_data['Team'] == team]
+        
+        if sub_data.shape[0] > 0: players_data[season] = sub_data['Name'].tolist()
+        
+    return players_data
+    
