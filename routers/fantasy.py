3cacd4bacc1039aacfa1dc8baa7600b748162603
Oghenefejiro Anigboro <Fejiroisaac@gmail.com>
2025-01-10 23:34:53 -0500

    added stats and transfers/update

--- routers/fantasy.py
+++ routers/fantasy.py
@@ -19,6 +19,16 @@ async def fantasy_loading(request: Request, session_token: str = Cookie(None)):
     return templates.TemplateResponse("fantasy.html", {"request": request, "user": user, 'data': get_user_data()})
 
 
+@router.post("/fantasy/update-team")
+async def update_team(request: Request, starting_team: str = Form(...), bench: str = Form(...)):
+    return templates.TemplateResponse("fantasy.html", {"request": request, "user": user, 'data': get_user_data()})
+
+
+@router.post("/fantasy/transfer-team")
+async def transfer_team(request: Request, my_player: str = Form(...), new_player: str = Form(...)):
+    return templates.TemplateResponse("fantasy.html", {"request": request, "user": user, 'data': get_user_data()})
+
+
 def get_user_data():
     dummy_data = {
         'Name': 'Dummy',
@@ -27,6 +37,19 @@ def get_user_data():
         'record': 62,
         'team': [('Rami', 25), ('Yass', 12), ('Will', 5), ('Fej', 17), ('Grant', -4)],
         'MW': 5,
-        'Deadline': '1/17'
+        'Deadline': '1/17',
+        'starting_team': [
+            {'name': 'Rami', 'pos': 'F', 'team': 'NPC', 'mw_points': 8, 'total_points': 40},
+            {'name': 'Yass', 'pos': 'M', 'team': 'NPC', 'mw_points': 7, 'total_points': 32},
+            {'name': 'Will', 'pos': 'D', 'team': 'NPC', 'mw_points': 6, 'total_points': 28},
+            {'name': 'Fej', 'pos': 'D', 'team': 'NPC', 'mw_points': 5, 'total_points': 35},
+            {'name': 'Grant', 'pos': 'GK', 'team': 'NPC', 'mw_points': -1, 'total_points': -15}
+        ],
+        'bench': [
+            {'name': 'Jake', 'pos': 'D', 'team': 'Team F', 'mw_points': 4, 'total_points': 20},
+            {'name': 'Leon', 'pos': 'M', 'team': 'Team G', 'mw_points': 3, 'total_points': 18},
+            {'name': 'Mike', 'pos': 'F', 'team': 'Team H', 'mw_points': 2, 'total_points': 10},
+        ]
     }
+
     return dummy_data
