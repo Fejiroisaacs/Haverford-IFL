307f2da6b2d33bc2b31d9d05dee78cf5964a9c93
Oghenefejiro Anigboro <Fejiroisaac@gmail.com>
2025-01-23 23:47:54 -0500

    updated contacts

--- routers/contact.py
+++ routers/contact.py
@@ -1,13 +1,12 @@
-from fastapi import FastAPI, Request, Depends, HTTPException, Form
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import Request, Form
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel
-import firebase_admin
-from fastapi.responses import RedirectResponse
-from starlette.responses import HTMLResponse, RedirectResponse
+from starlette.responses import HTMLResponse
 from fastapi import APIRouter
+from functions import send_email
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 router = APIRouter()
 
 templates = Jinja2Templates(directory="templates")
@@ -16,3 +15,25 @@ user = None
 @router.get("/contact", response_class=HTMLResponse)
 async def read_contact(request: Request):
     return templates.TemplateResponse("contact.html", {"request": request, "user": user})
+
+
+@router.post("/send-message", response_class=HTMLResponse)
+async def send_feedback(request: Request, email: str = Form(...), textarea: str = Form(...)):
+    try:
+        if not email or not textarea:
+            return templates.TemplateResponse("contact.html", {"request": request, 
+                                                            "error": 'All fields are required.', 
+                                                            "success": None})
+            
+        emails = ['fanigboro@haverford.edu', 'gdevries@haverford.edu']
+        if '@' in email:emails.append(email)
+        
+        send_email(email=f'{os.getenv("OUR_EMAIL")}', bccs=emails, subject=f'{email} left a message', message=textarea)
+        
+        return templates.TemplateResponse("contact.html", {"request": request, 
+                                                        "success": f"Thank you, {email}. We've received your message.", 
+                                                        "error": None})
+    except:
+        return templates.TemplateResponse("contact.html", {"request": request, 
+                                                        "success": None, 
+                                                        "error": 'Email not sent, something happened'})
