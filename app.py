da00168e2f911e5ab79b9567810d2dbf9d0a7ee7
Oghenefejiro Anigboro <Fejiroisaac@gmail.com>
2025-03-10 16:56:22 -0400

    .

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ async def sitemap():
     xml_content = """<?xml version="1.0" encoding="UTF-8"?>\n"""
     xml_content += """<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n"""
 
-    for url in set(urls):  # Ensure no duplicates
+    for url in set(urls):
         xml_content += f"""    <url>
         <loc>{url}</loc>
         <priority>0.8</priority>
